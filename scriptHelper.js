c879de05636b80cfd945e1894b1d8c93bf41f7d1
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-20 09:23:32 -0600

    added validate function

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,7 +17,9 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+  if (!testInput) return "Empty";
+  else if (typeof testInput !== 'number') return "Not a Number";
+  return "Is a Number";
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
